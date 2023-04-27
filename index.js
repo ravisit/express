66fdec0c9911006e0561262bf9eb8c0caea2e3f3
Ravi <ravi.1si14cs096@gmail.com>
2023-04-27 14:42:44 +0530

    Middleware used

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ var app = express();
 var route = require('./route');
 var dynamicRoute = require('./dynamicRoutes');
 
+//First middleware before response is sent
+app.use(function(req, res, next){
+   console.log('Middle-ware Start');
+   next();
+})
+
+//Route handler
 app.use('/', route);
 
 app.use('/', dynamicRoute);
+
+
 
 app.listen(3000);
